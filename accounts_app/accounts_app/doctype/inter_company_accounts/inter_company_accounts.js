d5224f7270629a0b0387ca4eff50c4a8de5dc284
hammad_abbasi <hammadsrca@gmail.com>
2024-08-07 08:49:21 +0000

    inter-company-accounts

---
+++ accounts_app/accounts_app/doctype/inter_company_accounts/inter_company_accounts.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, hammad and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Inter Company Accounts', {
+	// refresh: function(frm) {
+
+	// }
+});
